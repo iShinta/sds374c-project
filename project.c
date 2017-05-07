db8902a1f68c0c9218a1d4e62a1c9e8fba6994dd
iShinta <ho_minh_tri@yahoo.fr>
2017-05-07 10:40:08 -0500

    No star

--- project.c
+++ project.c
@@ -182,7 +182,7 @@ int main(){
     printf("Movie Counter: %i\n", counter);
     printf("Movie Average (Calcul): %d\n", (double)sum/(double)counter);
     movie_average_array[ii] = (double)sum/counter;
-    printf("Movie Average: %d\n", *movie_average_array[ii]);
+    printf("Movie Average: %d\n", movie_average_array[ii]);
   }
 
   printf("----- Calculate User Offsets -----\n");

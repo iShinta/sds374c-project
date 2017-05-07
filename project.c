8519c2e532f0ee4e1fafc6ff220ab1f8f2795230
iShinta <ho_minh_tri@yahoo.fr>
2017-05-07 14:01:26 -0500

    One time

--- project.c
+++ project.c
@@ -225,67 +225,64 @@ int main(){
   // Open the file
   FILE *fp2;
 
-  for(ii = 1; ii < nb_movies ; ii++){ // Starts at 1 because no movie 0
-    words = (char **)malloc(sizeof(char*)*lines_allocated);
-    if (words==NULL)
-    {
-       fprintf(stderr,"Out of memory (1).\n");
-       exit(1);
-    }
+  words = (char **)malloc(sizeof(char*)*lines_allocated);
+  if (words==NULL)
+  {
+     fprintf(stderr,"Out of memory (1).\n");
+     exit(1);
+  }
 
-    printf("------ Read Movie %i ------\n", ii);
-    fp2 = fopen("../project_data/probe.txt", "r");
-    if (fp2 == NULL)
-    {
-        fprintf(stderr,"Error opening file.\n");
-        exit(2);
-    }
+  fp2 = fopen("../project_data/probe.txt", "r");
+  if (fp2 == NULL)
+  {
+      fprintf(stderr,"Error opening file.\n");
+      exit(2);
+  }
 
-    printf("------ Read Lines ------\n");
-    // iterations until break
-    int i;
-    for (i=0;1;i++)
-    {
-        int j;
-        /* Have we gone over our line allocation? */
-        if (i >= lines_allocated)
-        {
-            int new_size;
+  printf("------ Read Lines ------\n");
+  // iterations until break
+  int i;
+  for (i=0;1;i++)
+  {
+      int j;
+      /* Have we gone over our line allocation? */
+      if (i >= lines_allocated)
+      {
+          int new_size;
 
-            /* Double our allocation and re-allocate */
-            new_size = lines_allocated*2;
-            words = (char **)realloc(words,sizeof(char*)*new_size);
-            if (words==NULL)
-            {
-                fprintf(stderr,"Out of memory.\n");
-                exit(3);
-            }
-            lines_allocated = new_size;
-        }
+          /* Double our allocation and re-allocate */
+          new_size = lines_allocated*2;
+          words = (char **)realloc(words,sizeof(char*)*new_size);
+          if (words==NULL)
+          {
+              fprintf(stderr,"Out of memory.\n");
+              exit(3);
+          }
+          lines_allocated = new_size;
+      }
 
-        /* Allocate space for the next line */
-        words[i] = malloc(max_line_len);
-        if (words[i]==NULL)
-        {
-            fprintf(stderr,"Out of memory (3).\n");
-            exit(4);
-        }
-        if (fgets(words[i],max_line_len-1,fp)==NULL)
-            break;
+      /* Allocate space for the next line */
+      words[i] = malloc(max_line_len);
+      if (words[i]==NULL)
+      {
+          fprintf(stderr,"Out of memory (3).\n");
+          exit(4);
+      }
+      if (fgets(words[i],max_line_len-1,fp)==NULL)
+          break;
 
-        /* Get rid of CR or LF at end of line */
-        for (j=strlen(words[i])-1;j>=0 && (words[i][j]=='\n' || words[i][j]=='\r');j--)
-          ;
-          words[i][j+1]='\0';
-    }
+      /* Get rid of CR or LF at end of line */
+      for (j=strlen(words[i])-1;j>=0 && (words[i][j]=='\n' || words[i][j]=='\r');j--)
+        ;
+        words[i][j+1]='\0';
+  }
 
-    /* Close file */
-    fclose(fp2);
+  /* Close file */
+  fclose(fp2);
 
-    int j;
-    for(j = 0; j < i; j++){
-      printf("%s\n", words[j]);
-    }
+  int j;
+  for(j = 0; j < i; j++){
+    printf("%s\n", words[j]);
   }
 
   printf("----- Program End -----\n");

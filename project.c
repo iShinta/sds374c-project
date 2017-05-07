4f2fc1ae4caf13cff93c55fbd01a2f9e320cddd4
iShinta <ho_minh_tri@yahoo.fr>
2017-05-07 10:34:39 -0500

    Calcul

--- project.c
+++ project.c
@@ -180,6 +180,7 @@ int main(){
     }
     printf("Movie Sum: %i\n", sum);
     printf("Movie Counter: %i\n", counter);
+    printf("Movie Average (Calcul): %d\n", (double)sum/counter;);
     movie_average_array[ii] = (double)sum/counter;
     printf("Movie Average: %d\n", movie_average_array[ii]);
   }
